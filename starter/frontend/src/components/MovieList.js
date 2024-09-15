9feecfdaa86f226bd8b9807f4ae9c59e4d70aa59
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2024-09-15 17:04:32 +0700

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,21 +6,15 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
-      .then((response) => {
-        setMovies(response.data.movies);
-      });
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
+      setMovies(response.data.movies);
+    });
   }, []);
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li
-          className='movieItem'
-          key={movie.id}
-          onClick={() => onMovieClick(movie)}
-        >
+        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
           {movie.title}
         </li>
       ))}
