b266fd6b11fa7f9e4af05a17252e9528c151ba54
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2024-09-15 17:36:06 +0700

    Update MovieDetails.js

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -4,11 +4,9 @@ import axios from 'axios';
 function MovieDetail({ movie }) {
   const [details, setDetails] = useState(null);
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`)
-      .then((response) => {
-        setDetails(response.data);
-      });
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
+      setDetails(response.data);
+    });
   }, [movie]);
 
   return (
